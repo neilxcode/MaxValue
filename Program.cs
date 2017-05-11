b41101f5cd95f2a5a444be870f88befeb63bd840
neilxcode <developros@icloud.com>
2017-05-10 23:39:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxValue
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Enter the amount of integers you will be providing:");
+            int numAmount = Convert.ToInt32(Console.ReadLine());
+
+            int []answer = new int[numAmount];
+            for(int i = 0; i < numAmount; i++)
+            {
+                Console.WriteLine("Enter int {0}:", i);           
+                answer[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maxNum = answer.Max();
+            Console.WriteLine("Max number is {0}!", maxNum);
+            
+
+        }
+    }
+}
